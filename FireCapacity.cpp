5641160852d57b955db22756de85bce2d7f3c2ca
katherine-fontan <luzfonta@chapman.edu>
2020-01-16 06:16:29 +0000

    else statement added to the program, no syntax errors

--- FireCapacity.cpp
+++ FireCapacity.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 int main (int argc, char **argv)
 {
-  int roomCapacity, numberPeople, difference;
+  int roomCapacity, numberPeople, difference, peopleRemoved;
 
   cout << "What is the maximum room capacity? " << endl;
   cin >> roomCapacity;
@@ -20,17 +20,22 @@ int main (int argc, char **argv)
   cout << "How many people will attend the meeting? " << endl;
   cin >> numberPeople;
 
+  difference = roomCapacity - numberPeople; //calculate the difference
   //if the number of people is less than or equal to the max room Capacity
     // announce that it is legal to hold the meeting
     //and how many additional people may legally attend
     if (numberPeople <= roomCapacity)
     {
       cout << "You can hold the meeting with the amount of people attending!" << endl;
-      difference = roomCapacity - numberPeople;
 
       cout << difference << " additional people may attend the meeting." << endl;
     }
-
+    else
+    {
+      peopleRemoved = difference * -1;
+      cout << "Sorry! The meeting cannot be held as plannes due to fire regulations." << endl;
+      cout << "Consider excluding " << peopleRemoved << " people in order to meet the fire regulations." << endl;
+    }
   //if the number of people exceed the maximum room capacity
     //announce that the meeting cannot be held as planned due to fire regulations
     //tells how many people must be excluded in orded to meet the fire regulations
