b304b3ed46c2c1acbf7fea46b33367681ffacbfa
katherine-fontan <luzfonta@chapman.edu>
2020-01-16 06:08:51 +0000

    started the if-else statements

---
+++ FireCapacity.cpp
@@ -0,0 +1,38 @@
+/*
+Katherine Fontan
+2290561
+Assignment 1- Fire Capacity
+
+Program determines whether a meeting room is in violation of fire law regulations
+regarding the maximum room capacity
+*/
+
+#include <iostream>
+using namespace std;
+
+int main (int argc, char **argv)
+{
+  int roomCapacity, numberPeople, difference;
+
+  cout << "What is the maximum room capacity? " << endl;
+  cin >> roomCapacity;
+
+  cout << "How many people will attend the meeting? " << endl;
+  cin >> numberPeople;
+
+  //if the number of people is less than or equal to the max room Capacity
+    // announce that it is legal to hold the meeting
+    //and how many additional people may legally attend
+    if (numberPeople <= roomCapacity)
+    {
+      cout << "You can hold the meeting with the amount of people attending!" << endl;
+      difference = roomCapacity - numberPeople;
+
+      cout << difference << " additional people may attend the meeting." << endl;
+    }
+
+  //if the number of people exceed the maximum room capacity
+    //announce that the meeting cannot be held as planned due to fire regulations
+    //tells how many people must be excluded in orded to meet the fire regulations
+
+}
